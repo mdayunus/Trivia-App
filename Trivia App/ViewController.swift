9c2e5c6ee9afa5ca57056faf6e870283531601df
mdayunus <mdayunus@gmail.com>
2019-10-10 23:34:21 +0530

    Initial Commit

---
+++ Trivia App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia App
+//
+//  Created by Mohammad Yunus on 10/10/19.
+//  Copyright © 2019 taptap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
